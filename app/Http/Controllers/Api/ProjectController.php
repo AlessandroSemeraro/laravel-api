ebef29a54b108b5dc836bcdda878589934058aa0
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-21 20:10:26 +0100

    created Api controller and added index and show

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+
+    $projects = Project::all();
+    
+    return response()->json($projects);
+
+    }
+
+
+    public function show(Project $project){
+
+        $projects = Project::all();
+        
+        return response()->json([$project]);
+    
+        }
+}
+
